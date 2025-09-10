f91cc9c61cf10a4798923ffdc8b1aa189f0ccf9d
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-10 19:34:52 +0200

    push (your hands up): INDEX BONUS

--- main.js
+++ main.js
@@ -24,7 +24,31 @@ const user = [
   { id: 3, name: 'Charlie' }
 ];
 
-// definiamo una route di tipo GET per ottenere tutti gli utenti
+/* definiamo una route di tipo GET per ottenere tutti gli utenti
+app.get('/users', (req, res) => {
+  res.send("mostro tutti gli utenti");
+});*/
+
+// BOUNS 
 app.get('/users', (req, res) => {
   res.json(user);
+});
+
+
+
+// iniziamo le operazioni CRUD -> "Create, Read, Update, Delete" (operazioni sulla risorsa) 
+// che corrispondono a ->  "INDEX, SHOW, CREATE, UPDATE, DELETE" (nomi delle operazioni/rotte)
+// ogni operazione corrisponde ad un metodo HTTP diverso (GET, POST, PUT, DELETE) -> i VERBI HTTP
+
+// INDEX -> ottenere tutti gli utenti (GET /users) -> già fatto sopra
+
+// definiamo una route di tipo GET per ottenere un utente specifico (SHOW)
+app.get('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const foundUser = user.find(u => u.id === userId);
+  if (foundUser) {
+    res.json(foundUser);
+  } else {
+    res.status(404).send('User not found');
+  }
 });
